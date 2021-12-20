7f5a083b7640668d72ad5ee03795d24cb850ab83
theng <theng2moak@gmail.com>
2021-12-20 16:19:07 +0700

    Laravel Role & Permission Modules

---
+++ database/seeds/CreateAdminUserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class CreateAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => 'Kn0ck@1234'
+        ]);
+
+        $role = Role::create(['name' => 'admin']);
+        $permissions = Permission::pluck('id', 'id')->all();
+        $role->syncPermissions($permissions);
+        $user->assignRole([$role->id]);
+    }
+}
