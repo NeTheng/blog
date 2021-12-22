eb40a9ef14b59b06765e9a87dee1f89ff0dfe58d
theng <theng2moak@gmail.com>
2021-12-22 11:17:53 +0700

    add province & district

---
+++ database/migrations/2021_12_22_024941_create_district_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDistrictTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('district', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('province_id');
+            $table->string('name', 70);
+            $table->timestamps();
+
+            $table->foreign('province_id')
+                ->references('id')
+                ->on('province')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('district');
+    }
+}
